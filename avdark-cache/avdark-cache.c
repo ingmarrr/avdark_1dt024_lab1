7873a246f679c387d7f0104397dbff2659259c83
Ingmar Falk <if222gq@student.lnu.se>
2025-02-11 10:47:41 +0100

    part 1

--- avdark-cache/avdark-cache.c
+++ avdark-cache/avdark-cache.c
@@ -1,14 +1,14 @@
 /**
-* Cache simulation using a functional system simulator.
-*
-* Course: Advanced Computer Architecture, Uppsala University
-* Course Part: Lab assignment 1
-*
-* Original authors: UART 1.0(?)
-* Modified by: Andreas Sandberg <andreas.sandberg@it.uu.se>
-*
-* $Id: avdark-cache.c 14 2011-08-24 09:55:20Z ansan501 $
-*/
+ * Cache simulation using a functional system simulator.
+ *
+ * Course: Advanced Computer Architecture, Uppsala University
+ * Course Part: Lab assignment 1
+ *
+ * Original authors: UART 1.0(?)
+ * Modified by: Andreas Sandberg <andreas.sandberg@it.uu.se>
+ *
+ * $Id: avdark-cache.c 14 2011-08-24 09:55:20Z ansan501 $
+ */
 
 #include "avdark-cache.h"
 
@@ -36,23 +36,23 @@
 #endif
 
 /**
-* Cache block information.
-*
-* HINT: You will probably need to change this structure
-*/
+ * Cache block information.
+ *
+ * HINT: You will probably need to change this structure
+ */
 struct avdc_cache_line {
-    avdc_tag_t  tag;
-    int         valid;
-    int         lru;
+    avdc_tag_t	tag;
+    int			valid;
+	int 		lru;
 };
 
 /**
-* Extract the cache line tag from a physical address.
-*
-* You probably don't want to change this function, instead you may
-* want to change how the tag_shift field is calculated in
-* avdc_resize().
-*/
+ * Extract the cache line tag from a physical address.
+ *
+ * You probably don't want to change this function, instead you may
+ * want to change how the tag_shift field is calculated in
+ * avdc_resize().
+ */
 static inline avdc_pa_t
 tag_from_pa(avdark_cache_t *self, avdc_pa_t pa)
 {
@@ -60,40 +60,40 @@ tag_from_pa(avdark_cache_t *self, avdc_pa_t pa)
 }
 
 /**
-* Calculate the cache line index from a physical address.
-*
-* Feel free to experiment and change this function
-*/
-static inline int
+ * Calculate the cache line index from a physical address.
+ *
+ * Feel free to experiment and change this function
+ */
+static inline int 
 index_from_pa(avdark_cache_t *self, avdc_pa_t pa)
 {
     return (pa >> self->block_size_log2) & (self->number_of_sets - 1);
 }
 
 /**
-* Computes the log2 of a 32 bit integer value. Used in dc_init
-*
-* Do NOT modify!
-*/
+ * Computes the log2 of a 32 bit integer value. Used in dc_init
+ *
+ * Do NOT modify!
+ */
 static int
 log2_int32(uint32_t value)
 {
     int i;
 
     for (i = 0; i < 32; i++) {
-        value >>= 1;
-        if (value == 0)
-            break;
+	value >>= 1;
+	if (value == 0)
+	    break;
     }
     return i;
 }
 
 /**
-* Check if a number is a power of 2. Used for cache parameter sanity
-* checks.
-*
-* Do NOT modify!
-*/
+ * Check if a number is a power of 2. Used for cache parameter sanity
+ * checks.
+ *
+ * Do NOT modify!
+ */
 static int
 is_power_of_two(uint64_t val)
 {
@@ -106,92 +106,105 @@ avdc_dbg_log(avdark_cache_t *self, const char *msg, ...)
     va_list ap;
 
     if (self->dbg) {
-        const char *name = self->dbg_name ? self->dbg_name : "AVDC";
-        fprintf(stderr, "[%s] dbg: ", name);
-        va_start(ap, msg);
-        vfprintf(stderr, msg, ap);
-        va_end(ap);
+	const char *name = self->dbg_name ? self->dbg_name : "AVDC";
+	fprintf(stderr, "[%s] dbg: ", name);
+	va_start(ap, msg);
+	vfprintf(stderr, msg, ap);
+	va_end(ap);
     }
 }
 
-// int
-// is_hit(avdark_cache_t* self, avdc_pa_t pa, avdc_tag_t tag)
-// {
-//     for (int ix = 0; ix < self->assoc; ++ix)
-//     {
-//         int index = index_from_pa(self, pa + ix);
-//         if (self->lines[index].valid && self->lines[index].tag == tag)
-//         {
-//             return index;
-//         }
-//     }
+int
+is_hit(avdark_cache_t* self, avdc_pa_t pa, avdc_tag_t tag, int* index)
+{
+	*index = index_from_pa(self, pa);
+	for (int ix = 0; ix < self->assoc; ++ix)
+	{
+		int hit = self->lines[(*index) + ix].valid && self->lines[(*index) + ix].tag == tag;
+		if (hit) {
+			*index += ix;
+			return hit;
+		}
+	}
 
-//     return -1;
-// }
+	int out_index = *index;
+
+	for (int ix = 1; ix < self->assoc; ++ix)
+	{
+		int ix_index = (*index) + ix;
+		if (self->lines[ix_index].lru < self->lines[out_index].lru)
+		{
+			out_index = ix_index;
+		}
+	}
+
+	self->lines[out_index].lru++;
+	*index = out_index;
+
+	return 0;
+}
 
 void
 avdc_access(avdark_cache_t *self, avdc_pa_t pa, avdc_access_type_t type)
 {
-    /* HINT: You will need to update this function */
-    avdc_tag_t tag = tag_from_pa(self, pa);
-    int index = index_from_pa(self, pa);
-    int hit;
-    hit = self->lines[index].valid && self->lines[index].tag == tag;
-    // int index = is_hit(self, pa, tag);
-    // int hit = index != -1;
+	self->dbg = 1;
 
-    if (!hit) {
-        self->lines[index].valid = 1;
-        self->lines[index].tag = tag;
-    }
+	avdc_tag_t tag = tag_from_pa(self, pa);
+	int index;
+	int hit = is_hit(self, pa, tag, &index);
 
-    switch (type) {
-        case AVDC_READ: /* Read accesses */
-        avdc_dbg_log(self, "read: pa: 0x%.16lx, tag: 0x%.16lx, index: %d, hit: %d\n",
-            (unsigned long)pa, (unsigned long)tag, index, hit);
-        self->stat_data_read += 1;
-        if (!hit)
-            self->stat_data_read_miss += 1;
-        break;
+	if (!hit) {
+		self->lines[index].valid = 1;
+		self->lines[index].tag = tag;
+	}
 
-        case AVDC_WRITE: /* Write accesses */
-        avdc_dbg_log(self, "write: pa: 0x%.16lx, tag: 0x%.16lx, index: %d, hit: %d\n",
-            (unsigned long)pa, (unsigned long)tag, index, hit);
-        self->stat_data_write += 1;
-        if (!hit)
-            self->stat_data_write_miss += 1;
-        break;
-    }
+	switch (type) {
+	case AVDC_READ: /* Read accesses */
+		avdc_dbg_log(self, "read: pa: 0x%.16lx, tag: 0x%.16lx, index: %d, hit: %d\n",
+			(unsigned long)pa, (unsigned long)tag, index, hit);
+		self->stat_data_read += 1;
+		if (!hit)
+			self->stat_data_read_miss += 1;
+		break;
+
+	case AVDC_WRITE: /* Write accesses */
+		avdc_dbg_log(self, "write: pa: 0x%.16lx, tag: 0x%.16lx, index: %d, hit: %d\n",
+			(unsigned long)pa, (unsigned long)tag, index, hit);
+		self->stat_data_write += 1;
+		if (!hit)
+			self->stat_data_write_miss += 1;
+		break;
+	}
 }
 
-void
+    void
 avdc_flush_cache(avdark_cache_t *self)
 {
-    /* HINT: You will need to update this function */
-    for (int i = 0; i < self->number_of_sets; i++) {
-        self->lines[i].valid = 0;
-        self->lines[i].tag = 0;
-    }
+	/* HINT: You will need to update this function */
+	for (int i = 0; i < self->number_of_sets; i++) {
+		self->lines[i].valid = 0;
+		self->lines[i].tag = 0;
+	}
 }
 
 
-int
+    int
 avdc_resize(avdark_cache_t *self,
-    avdc_size_t size, avdc_block_size_t block_size, avdc_assoc_t assoc)
+	avdc_size_t size, avdc_block_size_t block_size, avdc_assoc_t assoc)
 {
     /* HINT: This function precomputes some common values and
-    * allocates the self->lines array. You will need to update
-    * this to reflect any changes to how this array is supposed
-    * to be allocated.
-    */
+     * allocates the self->lines array. You will need to update
+     * this to reflect any changes to how this array is supposed
+     * to be allocated.
+     */
 
     /* Verify that the parameters are sane */
     if (!is_power_of_two(size) ||
-        !is_power_of_two(block_size) ||
-        !is_power_of_two(assoc)) {
-            fprintf(stderr, "size, block-size and assoc all have to be powers of two and > zero\n");
-            return 0;
-        }
+	    !is_power_of_two(block_size) ||
+	    !is_power_of_two(assoc)) {
+	fprintf(stderr, "size, block-size and assoc all have to be powers of two and > zero\n");
+	return 0;
+    }
 
     /* Update the stored parameters */
     self->size = size;
@@ -205,10 +218,10 @@ avdc_resize(avdark_cache_t *self,
 
     /* (Re-)Allocate space for the tags array */
     if (self->lines)
-        AVDC_FREE(self->lines);
+	AVDC_FREE(self->lines);
     /* HINT: If you change this, you may have to update
-    * avdc_delete() to reflect changes to how thie self->lines
-    * array is allocated. */
+     * avdc_delete() to reflect changes to how thie self->lines
+     * array is allocated. */
     self->lines = AVDC_MALLOC(self->number_of_sets, avdc_cache_line_t);
 
     /* Flush the cache, this initializes the tag array to a known state */
@@ -217,30 +230,30 @@ avdc_resize(avdark_cache_t *self,
     return 1;
 }
 
-void
+    void
 avdc_print_info(avdark_cache_t *self)
 {
     fprintf(stderr, "Cache Info\n");
     fprintf(stderr, "size: %d, assoc: %d, line-size: %d\n",
-        self->size, self->assoc, self->block_size);
+	    self->size, self->assoc, self->block_size);
 }
 
-void
+    void
 avdc_print_internals(avdark_cache_t *self)
 {
     int i;
 
     fprintf(stderr, "Cache Internals\n");
     fprintf(stderr, "size: %d, assoc: %d, line-size: %d\n",
-        self->size, self->assoc, self->block_size);
+	    self->size, self->assoc, self->block_size);
 
     for (i = 0; i < self->number_of_sets; i++)
-        fprintf(stderr, "tag: <0x%.16lx> valid: %d\n",
-            (long unsigned int)self->lines[i].tag,
-            self->lines[i].valid);
+	fprintf(stderr, "tag: <0x%.16lx> valid: %d\n",
+		(long unsigned int)self->lines[i].tag,
+		self->lines[i].valid);
 }
 
-void
+    void
 avdc_reset_statistics(avdark_cache_t *self)
 {
     self->stat_data_read = 0;
@@ -249,9 +262,9 @@ avdc_reset_statistics(avdark_cache_t *self)
     self->stat_data_write_miss = 0;
 }
 
-avdark_cache_t *
+    avdark_cache_t *
 avdc_new(avdc_size_t size, avdc_block_size_t block_size,
-    avdc_assoc_t assoc)
+	avdc_assoc_t assoc)
 {
     avdark_cache_t *self;
 
@@ -261,28 +274,29 @@ avdc_new(avdc_size_t size, avdc_block_size_t block_size,
     self->dbg = 0;
 
     if (!avdc_resize(self, size, block_size, assoc)) {
-        AVDC_FREE(self);
-        return NULL;
+	AVDC_FREE(self);
+	return NULL;
     }
 
     return self;
 }
 
-void
+    void
 avdc_delete(avdark_cache_t *self)
 {
     if (self->lines)
-        AVDC_FREE(self->lines);
+	AVDC_FREE(self->lines);
 
     AVDC_FREE(self);
 }
 
 /*
-* Local Variables:
-* mode: c
-* c-basic-offset: 8
-* indent-tabs-mode: nil
-* c-file-style: "linux"
-* compile-command: "make -k -C ../../"
-* End:
-*/
+ * Local Variables:
+ * mode: c
+ * c-basic-offset: 8
+ * indent-tabs-mode: nil
+ * c-file-style: "linux"
+ * compile-command: "make -k -C ../../"
+ * End:
+ */
+
